a05dddd961547019bb11a5ffd19ad9cfa463f257
HotManJerry <97427717+HotManJerry@users.noreply.github.com>
2024-10-06 21:41:45 +0500

    Add files via upload

---
+++ Builder/creators/patches.py
@@ -0,0 +1,19 @@
+import os
+
+class PatchSystemBugs:
+    @staticmethod
+    def enable_all_patches():
+        PatchSystemBugs.__fix_xterm_error_in_thunar()
+        PatchSystemBugs.__assign_permissions_to_configs()
+
+    @staticmethod
+    def __fix_xterm_error_in_thunar():
+        os.system("sudo ln -sf /usr/bin/gnome-terminal")
+
+    @staticmethod
+    def __make_fish_the_default():
+        os.system("chsh -s /usr/bin/fish")
+
+    @staticmethod
+    def __assign_permissions_to_configs():
+        os.system("sudo chmod -R 700 ~/.config/*")
---
+++ Builder/packages.py
@@ -0,0 +1,19 @@
+
+
+BASE_PACKAGES = [
+    "lsd", "gnome-terminal", "brightnessctl", "automake", "blueman", "bluez", "bluez-utils", "dunst", "fakeroot", "firefox", "dpkg", "gcc", "gnome-text-editor", "git", "gnu-netcat", "micro", "lxappearance", "mat2", "mpd", "mpv", "nemo", "network-manager-applet", "nitrogen", "tera-circle-icon-theme", "polybar", "autoconfautoconf", "mpc", "pulseaudio", "reflector", "rofi", "rofi-calc", "rofi-emoji", "sudo", "slop", "tree", "unrar", "zip", "unzip", "uthash", "feh", "xorg-xbacklight", "cmake", "clang", "gzip", "make", "openssh", "pulseaudio-bluetooth", "usbutils", "picom", "networkmanager-openvpn", "alsa-plugins", "alsa-tools", "alsa-utils", "p7zip", "gparted", "sshfs", "openvpn", "xclip", "gpick", "wget", "netctl", "light", "breeze", "vulkan-intel", "intel-ucode", "ttf-jetbrains-mono", "ttf-jetbrains-mono-nerd", "ttf-fira-code", "ttf-iosevka-nerd"
+]
+
+DEV_PACKAGES = [
+    "gtkhash", "gnome-firmware",  "telegram-desktop", "obsidian", "python-pip", "bpython", "ipython", 
+]
+
+AUR_PACKAGES = [
+    "cava", "i3lock-color", "ptpython"
+]
+
+GNOME_OFFICIAL_TOOLS = [
+    "gnome-calculator", "gucharmap",
+    "gthumb", "gnome-clocks"
+]
+
